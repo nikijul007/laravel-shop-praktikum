deb7cb21003faa0ae5e31b266614e19c1ceb927a
Marco <marco.ehrt@googlemail.com>
2017-07-09 09:22:32 +0200

    tabBild.png verschoben

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,9 +3,9 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>@yield('titel')</title> 
+        <title>@yield('titel')</title>
         <link rel="icon" href="tabBild2.ico">
-        <link rel="icon" href="../../../routes/tabBild.png">
+        <link rel="icon" href="{{ URL::asset('img/tabBild.png') }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel='stylesheet' href="{{ URL::to('scr/css/app.css') }}">
         <script src="https://use.fontawesome.com/da80c4bbaa.js"></script>
@@ -16,8 +16,8 @@
     </head>
     <body>
         @include('partials.header')
-       
-        
+
+
         <div class="container">
             @yield('inhalt')
         </div>
